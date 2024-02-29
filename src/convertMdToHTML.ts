ba001e286f2929005f98833c2b4268cd6bf8dbce
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-02-29 12:12:40 +0200

    added bold and italic convert functions

--- src/convertMdToHTML.ts
+++ src/convertMdToHTML.ts
@@ -1,5 +1,6 @@
-const replaceParagraphs = (md: string): string => {
-  const lines = md.split('\n');
+let errorMessage = '';
+
+const convertParagraph = (lines: string[]): string => {
   return lines.map((line) => {
     const isBold = line.startsWith('**');
     const isItalic = line.startsWith('_');
@@ -13,22 +14,51 @@ const replaceParagraphs = (md: string): string => {
   }).join('\n')
 }
 
+const convertBold = (lines: string[]): string => {
+  return lines.map((line, index) => {
+    const trimmedLine = line.trim();
+    const isBold = trimmedLine.startsWith('**') && trimmedLine.endsWith('**')
+    if (isBold) {
+      return `<b>${line.slice(2, line.length - 3)}</b>`;
+    } else if (trimmedLine.startsWith('**') && !trimmedLine.endsWith('**')) {
+      errorMessage += `In the line ${index + 1} the bold tag is not closed\n`;
+    }
+    return line;
+  }).join('\n')
+}
+
+const convertItalic = (lines: string[]): string => {
+  return lines.map((line, index) => {
+    const trimmedLine = line.trim();
+    const isItalic = trimmedLine.startsWith('_') && trimmedLine.endsWith('_')
+    if (isItalic) {
+      return `<i>${line.slice(1, line.length - 2)}</i>`;
+    } else if (trimmedLine.startsWith('_') && !trimmedLine.endsWith('_')) {
+      errorMessage += `In the line ${index + 1} the italic tag is not closed\n`;
+    }
+    return line;
+  }).join('\n')
+}
+
 const convertMdToHTML = (md: string): string => {
   let html = md;
+  const lines = md.split('\n');
 
-  html = replaceParagraphs(html);
+  html = convertParagraph(lines);
 
-  html = html.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+  html = convertBold(lines);
 
-  html = html.replace(/_(.*?)_/g, '<i>$1</i>');
+  html = convertItalic(lines);
 
   html = html.replace(/`(.*?)`/g, '<tt>$1</tt>');
 
   html = html.replace(/```(.*?)```/gs, '<pre>$1</pre>');
 
+  if (errorMessage) {
+    return errorMessage;
+  }
+
   return html;
 }
 
 export default convertMdToHTML;
-
-
