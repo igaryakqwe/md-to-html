dd8b5c9075704bb66a0170c2603d31ecfcfbab19
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-03-16 15:29:26 +0200

    changed readFile to readFileSync

--- src/fileReader.ts
+++ src/fileReader.ts
@@ -1,18 +1,20 @@
 import * as fs from "fs";
 import MarkdownConverter from "./convertMdToHTML";
 
-const readFile = (filePath: string): void => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      console.error("File or path has not found");
-      return;
-    }
+const readFile = (filePath: string): string => {
+  try {
+    const data = fs.readFileSync(filePath, "utf-8");
+
     if (!filePath.endsWith(".md")) {
       console.error("This is not a markdown file");
-      return;
+      return "";
     }
-    console.log(MarkdownConverter.convertMdToHTML(data));
-  });
+
+    return MarkdownConverter.convertMdToHTML(data);
+  } catch (err) {
+    console.error("File or path not found or cannot be read:", err);
+    return "";
+  }
 }
 
 export default readFile;
